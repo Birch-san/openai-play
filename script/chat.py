c16966066808503c2aaaa6d4a7e9d8b9c2199c25
Alex Birch <Birch-san@users.noreply.github.com>
2024-05-31 19:18:18 +0100

    migrate to newer OpenAI SDK. chat works, [legacy] completion doesn't.

--- script/chat.py
+++ script/chat.py
@@ -1,6 +1,11 @@
 import argparse
-import openai
 from typing import Literal
+from openai import OpenAI
+
+client = OpenAI(
+  api_key=open('key.txt', 'r').read().strip(),
+  organization='org-YOURORGHERE',
+)
 
 def main():
   # parse args:
@@ -10,31 +15,27 @@ def main():
 
   model: Literal['gpt-3.5-turbo-instruct', 'gpt-3.5-turbo', 'gpt-4', 'gpt-4-1106-preview', 'gpt-4-turbo', 'gpt-4-turbo-preview', 'gpt-4-turbo-2024-04-09', 'gpt-4o'] = args.model
 
-  openai.organization = "org-YOURORGHERE"
-  openai.api_key = open('key.txt', 'r').read().strip()
   # models = openai.Model.list()
   # [model.id for model in models.data]
 
   acc = ''
-  for chunk in openai.ChatCompletion.create(
+  for chunk in client.chat.completions.create(
     model=model,
     messages=[
       {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": f'''In PyTorch: how can I fuse a multiplicative factor into a matrix multiplication?'''},
-
-
+      {"role": "user", "content": f'''In PyTorch: how can I fuse a multiplicative factor into a matrix multiplication?'''},
     ],
     max_tokens=4096,
-    stream=True
+    stream=True,
   ):
     new_text = ''
-    delta = chunk['choices'][0].delta
-    if 'role' in delta:
-      role_raw = delta['role']
+    delta = chunk.choices[0].delta
+    if delta.role is not None:
+      role_raw = delta.role
       # capitalize first character of role
       role = role_raw[0].upper() + role_raw[1:]
       new_text += f'{role}: '
-    if 'content' in delta:
+    if delta.content is not None:
       new_text += delta.content
 
     print(new_text, end='', flush=True)
--- script/completion.py
+++ script/completion.py
@@ -1,29 +1,33 @@
 import argparse
-import openai
 from typing import Literal
+from openai import OpenAI
 
+client = OpenAI(
+  api_key=open('key.txt', 'r').read().strip(),
+  organization='org-YOURORGHERE',
+)
+
+# TODO: need to figure out how to get this working in the newer OpenAI SDK (it rejects these models as being chat models, but I think the older SDK allowed it).
 def main():
   # parse args:
   parser = argparse.ArgumentParser(description='Legacy completion API.')
   parser.add_argument('--model', default='gpt-4o', type=str, nargs='?', choices=['gpt-3.5-turbo-instruct', 'gpt-3.5-turbo', 'gpt-4', 'gpt-4o'], required=True)
   args = parser.parse_args()
 
-  model: Literal['gpt-3.5-turbo-instruct', 'gpt-3.5-turbo', 'gpt-4', 'gpt-4o'] = args.model
+  model: Literal['gpt-3.5-turbo-instruct', 'gpt-3.5-turbo', 'gpt-4'] = args.model
 
-  openai.organization = "org-YOURORGHERE"
-  openai.api_key = open('key.txt', 'r').read().strip()
   # models = openai.Model.list()
   # [model.id for model in models.data]
 
   acc = ''
-  for chunk in openai.Completion.create(
+  for chunk in client.completions.create(
     model=model,
     prompt="Say this is a test",
     max_tokens=512,
     temperature=0,
-    stream=True
+    stream=True,
   ):
-    new_text: str = chunk['choices'][0]['text']
+    new_text: str = chunk.choices[0].text
     print(new_text, end='', flush=True)
     acc += new_text
   pass
